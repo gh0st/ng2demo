a6adbc9823ec3cde1403fb782aa3a326e0cd9a81
Chad Hollman <hollmanchad@gmail.com>
2016-12-09 10:18:35 -0800

    forgot these files

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Person, SearchService } from '../shared/search/search.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit, OnDestroy {
+  query: string;
+  searchResults: Array<Person>;
+  sub: Subscription;
+
+  constructor(private searchService: SearchService, private router: Router, private route: ActivatedRoute) {
+    this.sub = this.route.params.subscribe(params => {
+      if (params['term']) {
+        this.query = decodeURIComponent(params['term']);
+        this.search();
+      }
+    });
+   }
+
+  search(): void {
+    this.searchService.search(this.query).subscribe(
+      data => { this.searchResults = data; },
+      error => console.log(error)
+    );
+  }
+
+  onSelect(person: Person) {
+    this.router.navigate(['/edit', person.id]);
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+}
